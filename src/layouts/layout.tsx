2adbc8ab0bef6d47083ce9364357e8e518c2fea1
pabin-limbu <changsu.pabin@gmail.com>
2024-07-30 19:20:28 +0800

    hero component removed form layout

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Hero from "@/components/Hero";
 
 type Props = {
   children: React.ReactNode;
@@ -11,7 +10,6 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
